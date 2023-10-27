0a5af37e68af38413e6e6e580e1e377e259a739e
unknown <ryanrich811@gmail.com>
2023-10-27 12:54:03 -0400

    Created two new relation tables for destinationChoices and Activities

--- SpookyVacation.sql
+++ SpookyVacation.sql
@@ -1,6 +1,21 @@
-CREATE TABLE IF NOT EXISTS FamilyMemmbers(
+CREATE TABLE IF NOT EXISTS FamilyMembers(
 	ID int,
     MemberAge int,
     MemberName varchar(20),
     primary key (ID)
+);
+
+CREATE TABLE IF NOT EXISTS DestinationChoices(
+	FamilyMemberID int,
+    DestinationName varchar(20),
+    DestinationZipCode int,
+    primary key (DestinationZipCode),
+    foreign key (FamilyMemberID) references FamilyMembers
+);
+
+CREATE TABLE IF NOT EXISTS Activities(
+	FamilyMemberID int,
+    Activity varchar(20),
+    primary key (FamilyMemberID),
+    foreign key (FamilyMemberID) references FamilyMembers
 );
