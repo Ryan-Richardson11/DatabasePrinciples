159681f87d1d16b76802cff81d2fcb0c2fbd3e74
unknown <ryanrich811@gmail.com>
2023-10-27 16:30:01 -0400

    Completed imports for all tables with unique values mathcing member ID's

--- SpookyVacation.sql
+++ SpookyVacation.sql
@@ -38,20 +38,20 @@ INSERT INTO FamilyMembers (MemberAge, MemberName) VALUES (75, "Mike");
 INSERT INTO FamilyMembers (MemberAge, MemberName) VALUES (82, "Mary");
 
 INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (1, "Salem, Massachusetts", 01915);
-INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (2, "Salem, Massachusetts", 01915);
-INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (3, "Salem, Massachusetts", 01915);
-INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (4, "Salem, Massachusetts", 01915);
-INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (5, "Salem, Massachusetts", 01915);
-INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (6, "Salem, Massachusetts", 01915);
-INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (7, "Salem, Massachusetts", 01915);
+INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (2, "Westford, Massachusetts", 01886);
+INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (3, "Salem, New Hampshire", 03079);
+INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (4, "Tyngsborough, Massachusetts", 01879);
+INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (5, "Tyngsborough, Massachusetts", 01879);
+INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (6, "Lowell, Massachusetts", 01850);
+INSERT INTO DestinationChoices (FamilyMemberID, DestinationName, DestinationZipCode) VALUES (7, "Freeport, Maine", 04013);
 
 INSERT INTO Activities (FamilyMemberID, Activity) VALUES (1, "Ghost Tour");
-INSERT INTO Activities (FamilyMemberID, Activity) VALUES (2, "Ghost Tour");
-INSERT INTO Activities (FamilyMemberID, Activity) VALUES (3, "Ghost Tour");
-INSERT INTO Activities (FamilyMemberID, Activity) VALUES (4, "Ghost Tour");
-INSERT INTO Activities (FamilyMemberID, Activity) VALUES (5, "Ghost Tour");
-INSERT INTO Activities (FamilyMemberID, Activity) VALUES (6, "Ghost Tour");
-INSERT INTO Activities (FamilyMemberID, Activity) VALUES (7, "Ghost Tour");
+INSERT INTO Activities (FamilyMemberID, Activity) VALUES (2, "Witches Woods");
+INSERT INTO Activities (FamilyMemberID, Activity) VALUES (3, "Screamfest");
+INSERT INTO Activities (FamilyMemberID, Activity) VALUES (4, "Trick-Or-Treating");
+INSERT INTO Activities (FamilyMemberID, Activity) VALUES (5, "Pumpkin Picking");
+INSERT INTO Activities (FamilyMemberID, Activity) VALUES (6, "Scary Movies");
+INSERT INTO Activities (FamilyMemberID, Activity) VALUES (7, "Camping with Ghost Stories");
 
 select * from FamilyMembers;
 select * from DestinationChoices;
