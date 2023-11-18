e597cc3cffb9965aba21213d195dc24713d21167
unknown <ryanrich811@gmail.com>
2023-11-18 15:25:06 -0500

    Added control flow logic for user interaction

--- Week4/Project4.py
+++ Week4/Project4.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def main():
+def selectRecipeFromCookbook():
 
     my_username = input("Please enter your username: ")
     my_password = input("Please enter your password: ")
@@ -30,5 +30,52 @@ def main():
     recipe_ingredients = cursor.fetchall()
     for i in recipe_ingredients:
         print(i)
+
+def saveRecipeToHolidayMenu(RecipeName, CookbookName, Ingredients, courseServed):
+    pass
+
+def main():
+
+    while True:
+        print("Select which action you would like to take: ")
+        print("===============================================")
+        print("1. View a recipe in a cookbook")
+        print("2. Get Account information and balance")
+        print("3. Change PIN")
+        print("4. Deposit money in account")
+        print("5. Transfer money between accounts")
+        print("6. Withdraw money from account")
+        print("7. ATM withdrawal")
+        print("8. Deposit Change")
+        print("9. Close an account")
+        print("10. Add monthly interest to all accounts")
+        print("11. Display balance in cents")
+        print("12. End Program")
+        print("===============================================")
+
+        choice = input("Enter your choice (1-11): ")
+
+        if choice == "1":
+           selectRecipeFromCookbook()
+
+        # elif choice == "2":
+            
+        # elif choice == "3":
+           
+        # elif choice == "4":
+            
+        # elif choice == "5":
+           
+        # elif choice == "6":
+            
+        # elif choice == "7":
+           
+        # elif choice == "8":
+            
+        elif choice == "9":
+            break
+        else:
+            print("Invalid choice \n")
+            continue
 
 main()
